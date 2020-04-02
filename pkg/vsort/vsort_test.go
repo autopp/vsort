fdc47d9f924758f0a406f145f6e0590b7da348c1
autopp <autopp.inc@gmail.com>
2020-04-02 20:36:51 +0900

    refactor: use sub test

--- pkg/vsort/vsort_test.go
+++ pkg/vsort/vsort_test.go
@@ -1,27 +1,44 @@
 package vsort
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestComparatorCompare(t *testing.T) {
-	cases := []struct {
+	type Case struct {
 		v1       string
 		v2       string
 		expected int
-	}{
+	}
+	cases := []Case{
 		{v1: "0.1.0", v2: "0.1.0", expected: 0},
 		{v1: "0.1.1", v2: "0.1.0", expected: 1},
 		{v1: "0.1.0", v2: "0.1.1", expected: -1},
 	}
 
-	for _, tt := range cases {
-		var c Comparator
-		actual, err := c.Compare(tt.v1, tt.v2)
-		if assert.NoError(t, err) {
-			assert.Equal(t, tt.expected, actual)
+	genSubtestName := func(c Case) string {
+		var op string
+		switch c.expected {
+		case 0:
+			op = "="
+		case 1:
+			op = ">"
+		case -1:
+			op = "<"
 		}
+		return fmt.Sprintf("%q%s%q", c.v1, op, c.v2)
+	}
+
+	for _, tt := range cases {
+		t.Run(genSubtestName(tt), func(t *testing.T) {
+			var c Comparator
+			actual, err := c.Compare(tt.v1, tt.v2)
+			if assert.NoError(t, err) {
+				assert.Equal(t, tt.expected, actual)
+			}
+		})
 	}
 }
