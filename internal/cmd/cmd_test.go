506006b82b96a7db2b8d7d2fa007d4d798c01177
autopp <autopp.inc@gmail.com>
2020-04-12 18:37:44 +0900

    refactor: nest test cases

--- internal/cmd/cmd_test.go
+++ internal/cmd/cmd_test.go
@@ -23,57 +23,59 @@ import (
 )
 
 func TestExecute(t *testing.T) {
-	cases := []struct {
-		input    string
-		args     []string
-		expected string
-	}{
-		{
-			input: `0.2.0
+	t.Run("WithStdin", func(t *testing.T) {
+		cases := []struct {
+			input    string
+			args     []string
+			expected string
+		}{
+			{
+				input: `0.2.0
 0.0.1
 0.10.0
 0.0.2`,
-			expected: `0.0.1
+				expected: `0.0.1
 0.0.2
 0.2.0
 0.10.0
 `,
-		},
-		{
-			input: `0.2.0
+			},
+			{
+				input: `0.2.0
 0.0.1
 0.10.0
 0.0.2
 `,
-			expected: `0.0.1
+				expected: `0.0.1
 0.0.2
 0.2.0
 0.10.0
 `,
-		},
-		{
-			input: `0.2.0
+			},
+			{
+				input: `0.2.0
 0.0.1
 0.10.0
 0.0.2`,
-			args: []string{"-r"},
-			expected: `0.10.0
+				args: []string{"-r"},
+				expected: `0.10.0
 0.2.0
 0.0.2
 0.0.1
 `,
-		},
-	}
+			},
+		}
 
-	for _, tt := range cases {
-		t.Run(fmt.Sprintf("%q", tt.args), func(t *testing.T) {
-			stdin := bytes.NewBufferString(tt.input)
-			stdout := new(bytes.Buffer)
-			stderr := new(bytes.Buffer)
+		for _, tt := range cases {
+			t.Run(fmt.Sprintf("%q", tt.args), func(t *testing.T) {
+				stdin := bytes.NewBufferString(tt.input)
+				stdout := new(bytes.Buffer)
+				stderr := new(bytes.Buffer)
 
-			if err := Execute(stdin, stdout, stderr, tt.args); assert.NoError(t, err) {
-				assert.Equal(t, tt.expected, stdout.String())
-			}
-		})
-	}
+				if err := Execute(stdin, stdout, stderr, tt.args); assert.NoError(t, err) {
+					assert.Equal(t, tt.expected, stdout.String())
+				}
+			})
+		}
+	})
 }
